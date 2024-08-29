8ec7c2d57feda1ba52049b85012cb97294fdcd5a
bautiarrua <bautista.arruabarrena@gmail.com>
2024-08-29 12:59:57 -0300

    main prueba

---
+++ src/main/java/MainPrueba.java
@@ -0,0 +1,10 @@
+import org.example.tici.TicIApplication;
+import org.springframework.boot.SpringApplication;
+
+public class MainPrueba {
+    public static void main(String[] args) {
+        for(int i=0; i<25; i++){
+            System.out.println(i);
+        }
+    }
+}
