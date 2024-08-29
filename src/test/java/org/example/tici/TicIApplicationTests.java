014b1ba2e036e962882fe42b328749685c39d2ee
bautiarrua <bautista.arruabarrena@gmail.com>
2024-08-29 12:53:53 -0300

    primer commit

---
+++ src/test/java/org/example/tici/TicIApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tici;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicIApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
