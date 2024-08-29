a1cb60323a57f39621f883c0a48caa53648f12e0
bautiarrua <bautista.arruabarrena@gmail.com>
2024-08-29 13:02:56 -0300

    probando

--- src/main/java/MainPrueba.java
+++ src/main/java/MainPrueba.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 public class MainPrueba {
     public static void main(String[] args) {
         for(int i=0; i<25; i++){
-            System.out.println(i);
+            System.out.println(i*100);
         }
     }
 }
