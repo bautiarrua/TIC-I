ddd6a92cac044d1f6705723897eb34c672ba0ebb
anamenoni <anamenoni@gmail.com>
2024-09-12 12:41:35 -0300

    hash contraseñ

--- src/main/java/org/example/tici/Service/UserService.java
+++ src/main/java/org/example/tici/Service/UserService.java
@@ -3,11 +3,15 @@ package org.example.tici.Service;
 import org.example.tici.Exceptions.YaExiste;
 import org.example.tici.Model.Entities.Users;
 import org.example.tici.Repository.UserRepository;
+import org.example.tici.config.SecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
+    public BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -16,6 +20,8 @@ public class UserService {
         if( userRepository.findByMail(user.getMail()) != null){
            throw new YaExiste();
         }
+        String hashedPassword = passwordEncoder.encode(user.getPasword());
+        user.setPasword(hashedPassword);
         userRepository.save(user);
         return user;
     }
